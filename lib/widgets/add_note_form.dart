ef933e0e411b6df8a9b8edb989837ba812b89a16
ahmed ibrahim <ahmedibrahim.ml13@gmail.com>
2024-06-24 16:43:57 +0300

    refactor multi bloc provider

--- lib/widgets/add_note_form.dart
+++ lib/widgets/add_note_form.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes_app/cubits/add_note_cubit/add_note_cubit.dart';
+import 'package:notes_app/models/note_model.dart';
 import 'package:notes_app/widgets/custom_button.dart';
 import 'package:notes_app/widgets/custom_text_field.dart';
 
@@ -15,7 +18,7 @@ class _AddNoteFormState extends State<AddNoteForm> {
   final GlobalKey<FormState> formKey = GlobalKey();
   AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
 
-  String? title, subtitle;
+  String? title, description;
 
   @override
   Widget build(BuildContext context) {
@@ -39,7 +42,7 @@ class _AddNoteFormState extends State<AddNoteForm> {
             hint: 'content',
             maxLines: 5,
             onSaved: (value) {
-              subtitle = value;
+              description = value;
             },
           ),
           const SizedBox(
@@ -50,6 +53,12 @@ class _AddNoteFormState extends State<AddNoteForm> {
             onTap: () {
               if (formKey.currentState!.validate()) {
                 formKey.currentState!.save();
+                var noteMode = NoteModel(
+                    title: title!,
+                    description: description!,
+                    date: DateTime.now().toString(),
+                    color: Colors.blue.value);
+                BlocProvider.of<AddNoteCubit>(context).addNote(noteMode);
               } else {
                 autovalidateMode = AutovalidateMode.always;
                 setState(() {});
