be4032f925ac2326e4762c4b1c844234dbf281f8
ahmed ibrahim <ahmedibrahim.ml13@gmail.com>
2024-06-25 14:52:05 +0300

    change add jnote loading

--- lib/widgets/custom_button.dart
+++ lib/widgets/custom_button.dart
@@ -2,30 +2,43 @@ import 'package:flutter/material.dart';
 import 'package:notes_app/constants/constants.dart';
 
 class CustomButton extends StatelessWidget {
-  const CustomButton({super.key, required this.text, this.onTap});
+  const CustomButton(
+      {super.key, required this.text, this.onTap, this.isLoading = false});
 
   final String text;
   final void Function()? onTap;
+  final bool isLoading;
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      onTap: onTap,
-      child: Container(
-        width: MediaQuery.of(context).size.width,
-        height: 55,
-        alignment: Alignment.center,
-        decoration: BoxDecoration(
-          color: kPrimaryColor,
-          borderRadius: BorderRadius.circular(16),
-        ),
-        child: Text(
-          text,
-          style: const TextStyle(
-            color: Colors.black,
-            fontWeight: FontWeight.bold,
-            fontSize: 18,
+    return AbsorbPointer(
+      absorbing: isLoading,
+      child: InkWell(
+        onTap: onTap,
+        child: Container(
+          width: MediaQuery.of(context).size.width,
+          height: 55,
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            color: kPrimaryColor,
+            borderRadius: BorderRadius.circular(16),
           ),
+          child: isLoading
+              ? const SizedBox(
+                  height: 24,
+                  width: 24,
+                  child: CircularProgressIndicator(
+                    color: Colors.black,
+                  ),
+                )
+              : Text(
+                  text,
+                  style: const TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                  ),
+                ),
         ),
       ),
     );
